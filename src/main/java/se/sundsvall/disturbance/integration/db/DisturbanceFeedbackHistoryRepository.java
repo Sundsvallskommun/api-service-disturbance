048a44a476adae52913efff65d12ec6c56c610f3
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-02 07:17:48 +0100

    UF-4484: Adjusted circuit breaker settings
    - Added circuitbreaker annotation on repository interfaces
    - Removed circuitbreaker annotation on feign client
    - Updated Dept44 to version 1.30

--- src/main/java/se/sundsvall/disturbance/integration/db/DisturbanceFeedbackHistoryRepository.java
+++ src/main/java/se/sundsvall/disturbance/integration/db/DisturbanceFeedbackHistoryRepository.java
@@ -6,9 +6,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import se.sundsvall.disturbance.integration.db.model.DisturbanceFeedbackHistoryEntity;
 
 @Transactional
+@CircuitBreaker(name = "disturbanceFeedbackHistoryRepository")
 public interface DisturbanceFeedbackHistoryRepository extends CrudRepository<DisturbanceFeedbackHistoryEntity, Long> {
 
 	List<DisturbanceFeedbackHistoryEntity> findByPartyId(String partyId);
