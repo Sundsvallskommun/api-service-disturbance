f114324db40fc768d86030d70de0665de6c173d4
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-12-17 10:48:08 +0100

    UF-11345: Add circuitbreaker-annotation

--- src/main/java/se/sundsvall/disturbance/integration/messaging/MessagingClient.java
+++ src/main/java/se/sundsvall/disturbance/integration/messaging/MessagingClient.java
@@ -1,17 +1,18 @@
 package se.sundsvall.disturbance.integration.messaging;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 import static se.sundsvall.disturbance.integration.messaging.configuration.MessagingConfiguration.CLIENT_ID;
 
 import generated.se.sundsvall.messaging.MessageRequest;
 import generated.se.sundsvall.messaging.MessageResult;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import se.sundsvall.disturbance.integration.messaging.configuration.MessagingConfiguration;
 
 @FeignClient(name = CLIENT_ID, url = "${integration.messaging.url}", configuration = MessagingConfiguration.class)
+@CircuitBreaker(name = CLIENT_ID)
 public interface MessagingClient {
 
 	/**
@@ -21,8 +22,6 @@ public interface MessagingClient {
 	 * @param  messageRequest with a list of messages.
 	 * @return                a MessageResult
 	 */
-	@PostMapping(path = "/{municipalityId}/messages?async=true", produces = {
-		APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE
-	}, consumes = APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/{municipalityId}/messages?async=true", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 	MessageResult sendMessage(@PathVariable("municipalityId") String municipalityId, MessageRequest messageRequest);
 }
