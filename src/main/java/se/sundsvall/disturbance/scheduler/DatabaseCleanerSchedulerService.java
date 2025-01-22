5d8e9fd41db2449c02451f5844d4d71a4dd6a8ee
cheezi0747 <linus.sjolinder@b3.se>
2025-01-22 13:27:01 +0100

    UF-11777: Use dept44-scheduler

--- src/main/java/se/sundsvall/disturbance/scheduler/DatabaseCleanerSchedulerService.java
+++ src/main/java/se/sundsvall/disturbance/scheduler/DatabaseCleanerSchedulerService.java
@@ -6,11 +6,10 @@ import static se.sundsvall.disturbance.api.model.Status.CLOSED;
 
 import java.time.OffsetDateTime;
 import java.util.List;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import se.sundsvall.dept44.scheduling.Dept44Scheduled;
 import se.sundsvall.disturbance.api.model.Status;
 import se.sundsvall.disturbance.integration.db.DisturbanceRepository;
 
@@ -21,23 +20,22 @@ public class DatabaseCleanerSchedulerService {
 
 	private static final List<Status> STATUSES_ELIGIBLE_FOR_REMOVAL = List.of(CLOSED);
 
-	private static final String LOG_CLEANING_STARTED = "Beginning removal of obsolete entities in the database.";
 	private static final String LOG_CLEANING_DELETE_RANGE = "Removing all disturbances older than '{}' and with status matching '{}'.";
-	private static final String LOG_CLEANING_ENDED = "Cleaning of obsolete entities in database has ended.";
 
 	private final DatabaseCleanerSchedulerProperties properties;
 	private final DisturbanceRepository disturbanceRepository;
 
-	public DatabaseCleanerSchedulerService(DatabaseCleanerSchedulerProperties properties, DisturbanceRepository disturbanceRepository) {
+	public DatabaseCleanerSchedulerService(final DatabaseCleanerSchedulerProperties properties, final DisturbanceRepository disturbanceRepository) {
 		this.properties = properties;
 		this.disturbanceRepository = disturbanceRepository;
 	}
 
-	@Scheduled(cron = "${scheduler.dbcleaner.cron:-}")
-	@SchedulerLock(name = "dbcleaner", lockAtMostFor = "${scheduler.dbcleaner.shedlock-lock-at-most-for}")
+	@Dept44Scheduled(
+		cron = "${scheduler.dbcleaner.cron:-}",
+		name = "${scheduler.dbcleaner.name}",
+		lockAtMostFor = "${scheduler.dbcleaner.shedlock-lock-at-most-for}",
+		maximumExecutionTime = "${scheduler.dbcleaner.maximum-execution-time}")
 	public void execute() {
-
-		LOGGER.info(LOG_CLEANING_STARTED);
 
 		final var statusesEligibleForRemoval = STATUSES_ELIGIBLE_FOR_REMOVAL.toArray(Status[]::new);
 		final var expiryDate = calculateExpiryDate();
@@ -45,8 +43,6 @@ public class DatabaseCleanerSchedulerService {
 		LOGGER.info(LOG_CLEANING_DELETE_RANGE, expiryDate, statusesEligibleForRemoval);
 
 		disturbanceRepository.deleteByCreatedBeforeAndStatusIn(expiryDate, statusesEligibleForRemoval);
-
-		LOGGER.info(LOG_CLEANING_ENDED);
 	}
 
 	private OffsetDateTime calculateExpiryDate() {
